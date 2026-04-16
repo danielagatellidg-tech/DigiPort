f252c59f4f4f710bdb5e85d8750e80b67309722a
danielagatellidg-tech <daniela.gatelli.dg@gmail.com>
2026-04-16 15:23:20 -0300

    tipos.comit1

---
+++ tipos.js
@@ -0,0 +1,11 @@
+let nome = "Daniela";
+console.log("Nome: ", nome)
+let sobrenome = "Gatelli";
+console.log("Sobrenome: ", sobrenome)
+let idade;
+idade =29
+console.log("Idade: ", idade)
+let cidade = "Novo Hamburgo";
+console.log("Cidade: ",cidade)
+let nota = 10;
+console.log("Nota: ",nota)
